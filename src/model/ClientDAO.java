c7359fb8224f7cf303c70f831d0ef9994110fa3b
xim xim <tlawogus11106@gmail.com>
2019-10-21 09:44:11 +0900

    clientDAO 수정함

--- src/model/ClientDAO.java
+++ src/model/ClientDAO.java
@@ -5,7 +5,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 import util.AlertManager;
 import util.AlertManager.AlertInfo;
@@ -13,12 +12,12 @@ import util.DBUtil;
 
 public class ClientDAO {
 
-	public void insertClientDB(ClientVO cvo) throws SQLException {
+	public void insertClientDB(ClientVO cvo) throws SQLException{
 
 		String dml = "insert into clientdb " + "(id,password,name,phone)" + " values " + "(?,?,?,?)";
 
 		try (Connection connection = DBUtil.getConnection();
-				PreparedStatement statement = connection.prepareStatement(dml);) {
+				PreparedStatement statement = connection.prepareStatement(dml);){
 			
 			statement.setString(1, cvo.getId());
 			statement.setString(2, cvo.getPassword());
@@ -26,6 +25,7 @@ public class ClientDAO {
 			statement.setString(4, cvo.getPhone());
 
 			statement.execute();
+
 		}
 
 	} // end of insertClientDB
@@ -33,38 +33,45 @@ public class ClientDAO {
 	public ArrayList<ClientVO> getClientCheck(ClientVO cvo) throws SQLException {
 
 		String dml = "select * from clientdb where id = ?";
-		String name2 = cvo.getId();
+
+		ArrayList<ClientVO> clientList = new ArrayList<ClientVO>();
 
 		try (Connection connection = DBUtil.getConnection();
-				PreparedStatement statement = connection.prepareStatement(dml);) {
-			statement.setString(1, name2);
+				PreparedStatement statement = connection.prepareStatement(dml);){
 			
-			try (ResultSet resultSet = statement.executeQuery();) {
-				return getClientInfoFromResultSet(resultSet);
+			statement.setString(1, cvo.getId());
+			ResultSet resultSet = statement.executeQuery();
+			
+			while (resultSet.next()) {
+				ClientVO clientVo = new ClientVO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4));
+				clientList.add(clientVo);
 			}
-		}
-	}
+		} 
+		
+		return clientList;
 
-	public ArrayList<ClientVO> getClientInfo() throws SQLException {
+	} // end of getClientCheck
+
+	public ArrayList<ClientVO> getClientInfo() throws SQLException{
 
 		String dml = "select * from clientdb";
-		
-		try (Connection connection = DBUtil.getConnection();
-				PreparedStatement statement = connection.prepareStatement(dml);
-				ResultSet resultSet = statement.executeQuery();) {
-			return getClientInfoFromResultSet(resultSet);
-		}
-	}
-	
-	private ArrayList<ClientVO> getClientInfoFromResultSet(ResultSet resultSet) throws SQLException {
 		ArrayList<ClientVO> clientList = new ArrayList<ClientVO>();
-		while (resultSet.next()) {
-			ClientVO client = new ClientVO(resultSet.getString(1), 
-					resultSet.getString(2), 
-					resultSet.getString(3), 
-					resultSet.getString(4));
-			clientList.add(client);
+
+		try (Connection connection = DBUtil.getConnection();
+				PreparedStatement statement = connection.prepareStatement(dml)){
+
+			ResultSet resultSet = statement.executeQuery();
+			
+			while (resultSet.next()) {
+
+				ClientVO clientVo = new ClientVO(resultSet.getString(1), resultSet.getString(2), resultSet.getString(3), resultSet.getString(4));
+				clientList.add(clientVo);
+				
+			}
 		}
+		
 		return clientList;
+
 	}
+
 }
